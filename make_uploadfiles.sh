c865c435fb34b0173da7d17eaca058a2f40a0ef9
mark-b1980 <mark.b@post.cz>
2024-11-01 17:13:00 +0100

    Copy script to prepre file for uploading with various extensions

---
+++ make_uploadfiles.sh
@@ -0,0 +1,14 @@
+mkdir ready2upload
+cp phpminal.php ready2upload/t.php
+cp phpminal.php ready2upload/t.php3
+cp phpminal.php ready2upload/t.php4
+cp phpminal.php ready2upload/t.php5
+cp phpminal.php ready2upload/t.php7
+cp phpminal.php ready2upload/t.php8
+cp phpminal.php ready2upload/t.pht
+cp phpminal.php ready2upload/t.phtm
+cp phpminal.php ready2upload/t.phtml
+cp phpminal.php ready2upload/t.phar
+cp phpminal.php ready2upload/t.phpt
+cp phpminal.php ready2upload/t.phps
+cp phpminal.php ready2upload/t.jpg.php
